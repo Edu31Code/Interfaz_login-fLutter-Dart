508bec12427c6a6f9353a5d9a4b58b12b1119099
Edu31Code <gerencia@ingealimite.com>
2021-06-07 11:10:55 -0500

    “Evaluacion”

--- android/app/src/main/kotlin/com/example/evaluacion_itsqmet_benavides/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.evaluacion_itsqmet_benavides
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/ui/widgets/auth_form.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:login_register_auth/ui/screens/auth_screen.dart';
+import 'package:login_register_auth/ui/widgets/original_button.dart';
+import 'package:login_register_auth/services/auth.dart';
+
+class AuthForm extends StatefulWidget {
+  final AuthType authType;
+
+  const AuthForm({Key key, @required this.authType}) : super(key: key);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '', _password = '';
+  AuthBase authBase = AuthBase();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'email',
+                hintText: 'ex: ebenavides@gmail.com',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+              validator: (value) =>
+                  value.isEmpty ? 'Debes ingresar un correo electrónico válido' : null,
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'password',
+              ),
+              obscureText: true,
+              onChanged: (value) {
+                _password = value;
+              },
+              validator: (value) => value.length <= 6
+                  ? 'ingrese 8 caracteres'
+                  : null,
+            ),
+            SizedBox(height: 20),
+            OriginalButton(
+              text: widget.authType == AuthType.login ? 'Login in' : 'Registrar',
+              color: Colors.lightBlue,
+              textColor: Colors.white,
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  if (widget.authType == AuthType.login) {
+                    await authBase.loginWithEmailAndPassword(_email, _password);
+                    Navigator.of(context).pushReplacementNamed('home');
+                  } else {
+                    await authBase.registerWithEmailAndPassword(_email, _password);
+                    Navigator.of(context).pushReplacementNamed('home');
+                  }
+                }
+              },
+            ),
+            SizedBox(height: 6),
+            FlatButton(
+              onPressed: () {
+                if (widget.authType == AuthType.login) {
+                  Navigator.of(context).pushReplacementNamed('register');
+                  print(widget.authType);
+                } else {
+                  Navigator.of(context).pushReplacementNamed('login');
+                }
+              },
+              child: Text(
+                widget.authType == AuthType.login
+                    ? 'No tienes una cuenta?'
+                    : 'Ya tienes una cuenta?',
+                style: TextStyle(fontSize: 18, color: Colors.black54),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
